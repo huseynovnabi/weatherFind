d8599f9562f41ad53ba85ce625ee6ac7c3187e39
huseynovnabi huseynovnabi <huseynovnabi2002@gmail.com>
2024-01-06 13:38:10 +0000

    change

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 "use strict";
 $(document).ready(function () {
-    $(document).on("keypress", function (e) {
+    $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=f7705d85d6989382feac2dfd15dbec1f&units=metric
-            `;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=f7705d85d6989382feac2dfd15dbec1f&units=metric`;
             async function myweather() {
                 let responce = await fetch(url);
                 let data = await responce.json();
@@ -20,8 +19,7 @@ $(document).ready(function () {
 
     $("#btn").click(function (e) {
         e.preventDefault();
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=f7705d85d6989382feac2dfd15dbec1f&units=metric
-        `;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=f7705d85d6989382feac2dfd15dbec1f&units=metric`;
         async function myweather() {
             let responce = await fetch(url);
             let data = await responce.json();
